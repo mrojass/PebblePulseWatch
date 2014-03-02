c50cb2ff54d3c030447bdfa0efb57b92a4c839ad
Michelle <mrojass1028@gmail.com>
2014-03-01 22:20:51 -0500

    button presses switch statement

--- src/pebble_pulse.c
+++ src/pebble_pulse.c
@@ -6,25 +6,23 @@ static TextLayer *text_layer;
 
 // Key values for AppMessage Dictionary
 enum {
-	STATUS_KEY = 0,	
-	MESSAGE_KEY = 1
-};
-
-enum {
-	RECEIVE_KEY = 0,
-	SEND_KEY = 1
+    STATUS_KEY = 0, 
+    MESSAGE_KEY = 1
 };
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Message Sent!");
+  send_message(BUTTON_ID_SELECT);
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Up");
+  send_message(BUTTON_ID_UP);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Down");
+  send_message(BUTTON_ID_DOWN);
 }
 
 static void click_config_provider(void *context) {
@@ -34,43 +32,38 @@ static void click_config_provider(void *context) {
 }
 
 // Write message to buffer & send
-void send_message(void){
-	DictionaryIterator *iter;
-	
-	app_message_outbox_begin(&iter);
-	dict_write_uint8(iter, STATUS_KEY, 0x1);
-	dict_write_cstring(iter, MESSAGE_KEY, "Hi Phone, I'm a Pebble!");
-	//while(click_recognizer_get_button_id(select_click_handler) != BUTTON_ID_SELECT)
-    //{
-	    if(click_recognizer_get_button_id(select_click_handler) == BUTTON_ID_UP) {
-	      dict_write_cstring(iter, MESSAGE_KEY, ".");
-	    } else if (click_recognizer_get_button_id(select_click_handler) == BUTTON_ID_DOWN) {
-	      dict_write_cstring(iter, MESSAGE_KEY, "-");
-	    } else {
-	      dict_write_cstring(iter, MESSAGE_KEY, "\nInvalid Button\n");
-	      //break;
-		}
-	//}
-	dict_write_end(iter);
-  	app_message_outbox_send();
+void send_message(ButtonId button_pressed){
+    DictionaryIterator *iter;
+    
+    app_message_outbox_begin(&iter);
+    dict_write_uint8(iter, STATUS_KEY, 0x1);
+    dict_write_cstring(iter, MESSAGE_KEY, "Hi Phone, I'm a Pebble!");
+    switch(button_pressed){
+        case BUTTON_ID_UP: dict_write_cstring(iter, MESSAGE_KEY, "."); break;
+        case BUTTON_ID_DOWN: dict_write_cstring(iter, MESSAGE_KEY, "-"); break;
+        case BUTTON_ID_SELECT: return;
+        default: dict_write_cstring(iter, MESSAGE_KEY, "\nInvalid Button\n"); break;
+
+    dict_write_end(iter);
+    app_message_outbox_send();
 }
 
 // Called when a message is received from PebbleKitJS
 static void in_received_handler(DictionaryIterator *received, void *context) {
-	Tuple *tuple;
-	
-	tuple = dict_find(received, STATUS_KEY);
-	if(tuple) {
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
-	}
-	
-	tuple = dict_find(received, MESSAGE_KEY);
-	if(tuple) {
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
-	}}
+    Tuple *tuple;
+    
+    tuple = dict_find(received, STATUS_KEY);
+    if(tuple) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
+    }
+    
+    tuple = dict_find(received, MESSAGE_KEY);
+    if(tuple) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
+    }}
 
 // Called when an incoming message from PebbleKitJS is dropped
-static void in_dropped_handler(AppMessageResult reason, void *context) {	
+static void in_dropped_handler(AppMessageResult reason, void *context) {    
 }
 
 // Called when PebbleKitJS does not acknowledge receipt of a message
@@ -92,32 +85,32 @@ static void window_unload(Window *window) {
 }
 
 void init(void) {
-	window = window_create();
-	window_set_click_config_provider(window, click_config_provider);
+    window = window_create();
+    window_set_click_config_provider(window, click_config_provider);
     window_set_window_handlers(window, (WindowHandlers) {
-    	.load = window_load,
-    	.unload = window_unload,
+        .load = window_load,
+        .unload = window_unload,
     });
     // const bool animated = true;
-	window_stack_push(window, true);
-	
-	// Register AppMessage handlers
-	app_message_register_inbox_received(in_received_handler); 
-	app_message_register_inbox_dropped(in_dropped_handler); 
-	app_message_register_outbox_failed(out_failed_handler);
-		
-	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-	
-	send_message();
+    window_stack_push(window, true);
+    
+    // Register AppMessage handlers
+    app_message_register_inbox_received(in_received_handler); 
+    app_message_register_inbox_dropped(in_dropped_handler); 
+    app_message_register_outbox_failed(out_failed_handler);
+        
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
+    send_message();
 }
 
 void deinit(void) {
-	app_message_deregister_callbacks();
-	window_destroy(window);
+    app_message_deregister_callbacks();
+    window_destroy(window);
 }
 
 int main( void ) {
-	init();
-	app_event_loop();
-	deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
